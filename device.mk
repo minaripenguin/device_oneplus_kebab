2fe8cca356ab0e2ba1367dbe22b4632ed49958a9
Eamon Powell <eamonpowell@outlook.com>
2021-11-16 18:57:21 -0800

    kebab: Add task profiles for API 31
    This is necessary in order for stune boost to work.
    
    Change-Id: Icc3444be95a177fb4b6ef77cbf582d696381b9cb
    Signed-off-by: strwbrry143 <alexfinhart@gmail.com>

--- device.mk
+++ device.mk
@@ -7,6 +7,11 @@
 # A/B
 $(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota.mk)
 
+# Task profiles
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/task_profiles/cgroups_31.json:$(TARGET_COPY_OUT_SYSTEM)/etc/task_profiles/cgroups_31.json \
+    $(LOCAL_PATH)/task_profiles/task_profiles_31.json:$(TARGET_COPY_OUT_SYSTEM)/etc/task_profiles/task_profiles_31.json
+
 AB_OTA_POSTINSTALL_CONFIG += \
     RUN_POSTINSTALL_system=true \
     POSTINSTALL_PATH_system=system/bin/otapreopt_script \
