f4c818251cd7f151df73a8e87a47b7ae4cd1cb3d
strwbrry143 <alexfinhart@gmail.com>
2022-09-12 20:38:21 +0800

    kebab: set properties for riceDroid 13.0

--- init/init_oplus_kona.cpp
+++ init/init_oplus_kona.cpp
@@ -152,4 +152,7 @@ void vendor_load_properties() {
     set_ro_build_prop("model", model);
     set_ro_build_prop("name", name);
     set_ro_build_prop("product", model, false);
+    property_override("ro.rice.chipset", "Snapdragon 870 5G");
+    property_override("ro.rice.maintainer", "chaeyo.0");
+    property_override("ro.rice.version", "IceCream");
 }
