94f52e6fcd55170ca711db472792c1d29caf133e
strwbrry143 <alexfinhart@gmail.com>
2022-09-20 06:35:25 +0800

    kebab: Inherit dalvik heap config for 8gb devices

--- device.mk
+++ device.mk
@@ -132,7 +132,7 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.camera.raw.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.raw.xml
 
 # Dalvik
-$(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
+$(call inherit-product, frameworks/native/build/phone-xhdpi-8192-dalvik-heap.mk)
 
 # Device ID attestation
 PRODUCT_COPY_FILES += \
