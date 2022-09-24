c6ec65c8dffbaec818a1f58fca0b2fa5ff3079eb
strwbrry143 <alexfinhart@gmail.com>
2022-09-24 09:04:34 +0800

    kebab: Properly ship task profiles and cgroups
    * task profiles/cgroups.json taken from qssi-user-13-TP1A.220905.001-R.a2f826

--- device.mk
+++ device.mk
@@ -9,10 +9,8 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota.mk)
 
 # Task profiles
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/task_profiles/cgroups_31.json:$(TARGET_COPY_OUT_SYSTEM)/etc/task_profiles/cgroups_31.json \
-    $(LOCAL_PATH)/task_profiles/task_profiles_31.json:$(TARGET_COPY_OUT_SYSTEM)/etc/task_profiles/task_profiles_31.json \
-    $(LOCAL_PATH)/task_profiles/cgroups_31.json:$(TARGET_COPY_OUT_VENDOR)/etc/cgroups_31.json \
-    $(LOCAL_PATH)/task_profiles/task_profiles_31.json:$(TARGET_COPY_OUT_VENDOR)/etc/task_profiles_31.json
+    $(LOCAL_PATH)/task_profiles/cgroups.json:$(TARGET_COPY_OUT_VENDOR)/etc/cgroups.json \
+    $(LOCAL_PATH)/task_profiles/task_profiles.json:$(TARGET_COPY_OUT_VENDOR)/etc/task_profiles.json
 
 AB_OTA_POSTINSTALL_CONFIG += \
     RUN_POSTINSTALL_system=true \
